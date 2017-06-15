ef9477c7a3eef25adc32bf5994d6661ec3b1e2c8
Varun-Bhatia <varun.bhatia61@gmail.com>
2017-06-15 17:31:25 +0530

    Create DBconn.php

--- DBconn.php
+++ DBconn.php
@@ -1,7 +1,7 @@
 <?php
 function connfn()
 {
-$link =  mysqli_connect("localhost", "root", "root", "test");
+$link =  mysqli_connect("localhost", "mailhost", "mailhost", "mailhost");
 if($link === false)
 {
     die("ERROR: Could not connect. " . mysqli_connect_error());
